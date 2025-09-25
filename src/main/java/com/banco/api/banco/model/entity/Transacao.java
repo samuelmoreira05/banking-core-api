afbccea2c3490dc5e7c5f877ff3e7a5a564d3584
samuelmoreira05 <samuel442005@gmail.com>
2025-09-24 23:06:59 -0300

    feat: criacao entidade Transacao com relacionamento para Conta

---
+++ src/main/java/com/banco/api/banco/model/entity/Transacao.java
@@ -0,0 +1,41 @@
+package com.banco.api.banco.model.entity;
+
+import com.banco.api.banco.enums.TipoTransacao;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Transacao")
+@Table(name = "transacoes")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_transacao")
+    private TipoTransacao tipo;
+
+    @CreationTimestamp
+    private LocalDateTime dataTransacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+    private BigDecimal valor;
+
+    private BigDecimal saldoAnterior;
+
+
+}
