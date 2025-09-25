c9e386cc741ed20cda48888822ba338963d47003
samuelmoreira05 <samuel442005@gmail.com>
2025-09-24 23:58:58 -0300

    feat: implementacao dos DTOs de resposta para transacao

---
+++ src/main/java/com/banco/api/banco/controller/transacao/response/DadosContaResumida.java
@@ -0,0 +1,6 @@
+package com.banco.api.banco.controller.transacao.response;
+
+public record DadosContaResumida(
+        String numeroConta,
+        String agencia
+) {}
--- src/main/java/com/banco/api/banco/controller/transacao/response/DadosMostrarTransacaoResponse.java
+++ src/main/java/com/banco/api/banco/controller/transacao/response/DadosMostrarTransacaoResponse.java
@@ -1,4 +1,30 @@
 package com.banco.api.banco.controller.transacao.response;
 
-public record DadosMostrarTransacaoResponse() {
+import com.banco.api.banco.enums.TipoTransacao;
+import com.banco.api.banco.model.entity.Conta;
+import com.banco.api.banco.model.entity.Transacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record DadosMostrarTransacaoResponse(
+        Long idTransacao,
+        TipoTransacao tipo,
+        LocalDateTime dataTransacao,
+        BigDecimal valor,
+        DadosContaResumida conta
+) {
+    public DadosMostrarTransacaoResponse(Transacao transacao) {
+        this(
+                transacao.getId(),
+                transacao.getTipo(),
+                transacao.getDataTransacao(),
+                transacao.getValor(),
+                new DadosContaResumida(
+                        transacao.getConta().getNumeroConta(),
+                        transacao.getConta().getAgencia()
+                )
+        );
+    }
 }
+
