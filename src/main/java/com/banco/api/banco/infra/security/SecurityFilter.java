f10306e5cb07038426936b34f2aa0951cae6061b
samuelmoreira05 <samuel442005@gmail.com>
2025-09-29 21:37:51 -0300

    fix: correcao para validar se o token existe/valido

--- src/main/java/com/banco/api/banco/infra/security/SecurityFilter.java
+++ src/main/java/com/banco/api/banco/infra/security/SecurityFilter.java
@@ -1,4 +1,39 @@
 package com.banco.api.banco.infra.security;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenSecurity security;
+
+    public SecurityFilter(TokenSecurity security) {
+        this.security = security;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var jwtToken = verificarToken(request);
+        var subject = security.getSubject(jwtToken);
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String verificarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("O token não foi enviado");
+        }
+        return authorizationHeader.replace("Bearer", "");
+    }
+
+
 }
--- src/main/java/com/banco/api/banco/infra/security/TokenSecurity.java
+++ src/main/java/com/banco/api/banco/infra/security/TokenSecurity.java
@@ -3,7 +3,9 @@ package com.banco.api.banco.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.banco.api.banco.model.entity.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,9 +16,12 @@ import java.util.Date;
 @Service
 public class TokenSecurity {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String geraToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("banking-core-api")
                     .withSubject(usuario.getLogin())
@@ -25,6 +30,19 @@ public class TokenSecurity {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("banking-core-api")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
