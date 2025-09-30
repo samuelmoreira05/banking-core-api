10ff0ed45ef03481f16f71348331632198396ebc
samuelmoreira05 <samuel442005@gmail.com>
2025-09-30 20:37:15 -0300

    fix: autententicando usuario e testando controle de acesso

--- src/main/java/com/banco/api/banco/infra/security/SecurityFilter.java
+++ src/main/java/com/banco/api/banco/infra/security/SecurityFilter.java
@@ -1,9 +1,12 @@
 package com.banco.api.banco.infra.security;
 
+import com.banco.api.banco.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,27 +15,35 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    private final TokenSecurity security;
+    private final TokenSecurity tokenSecurity;
+    private final UsuarioRepository repository;
 
-    public SecurityFilter(TokenSecurity security) {
-        this.security = security;
+    public SecurityFilter(TokenSecurity security, UsuarioRepository repository) {
+        this.tokenSecurity = security;
+        this.repository = repository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var jwtToken = verificarToken(request);
-        var subject = security.getSubject(jwtToken);
+        if (jwtToken != null){
+            var subject = tokenSecurity.getSubject(jwtToken);
+            var usuario = repository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.get().getAuthorities());
 
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String verificarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw new RuntimeException("O token não foi enviado");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer", "").trim();
         }
-        return authorizationHeader.replace("Bearer", "");
+
+        return null;
     }
 
 
