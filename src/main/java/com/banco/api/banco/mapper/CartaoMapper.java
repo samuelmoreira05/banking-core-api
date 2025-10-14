8ce6686c0e38daeb14f38827b1e9b5acd09abd98
samuelmoreira05 <samuel442005@gmail.com>
2025-10-14 16:23:03 -0300

    feat: logica de build do cartao para dentro do mapper CartaoMapper

--- src/main/java/com/banco/api/banco/mapper/CartaoMapper.java
+++ src/main/java/com/banco/api/banco/mapper/CartaoMapper.java
@@ -1,4 +1,24 @@
 package com.banco.api.banco.mapper;
 
+import com.banco.api.banco.controller.cartao.request.CartaoDebitoCriarDadosRequest;
+import com.banco.api.banco.enums.TipoCartao;
+import com.banco.api.banco.model.entity.Cartao;
+import com.banco.api.banco.model.entity.Conta;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
 public class CartaoMapper {
+
+    public Cartao toEntity(CartaoDebitoCriarDadosRequest dados, Conta conta) {
+        Cartao cartao = Cartao.builder()
+                .conta(conta)
+                .tipoCartao(TipoCartao.DEBITO)
+                .build();
+
+        return cartao;
+    }
 }
