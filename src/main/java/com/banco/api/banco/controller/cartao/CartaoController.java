0ec69aaed89c36ac0bff6ecf9ac38a16fe55337e
samuelmoreira05 <samuel442005@gmail.com>
2025-10-15 16:17:37 -0300

    feat: criacao da controller de cartoes

--- src/main/java/com/banco/api/banco/controller/cartao/CartaoController.java
+++ src/main/java/com/banco/api/banco/controller/cartao/CartaoController.java
@@ -1,4 +1,33 @@
 package com.banco.api.banco.controller.cartao;
 
+import com.banco.api.banco.controller.cartao.request.CartaoDebitoCriarDadosRequest;
+import com.banco.api.banco.controller.cartao.response.CartaoDebitoMostrarDadosResponse;
+import com.banco.api.banco.service.CartaoService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cartoes")
 public class CartaoController {
+
+    private final CartaoService cartaoService;
+
+    public CartaoController(CartaoService cartaoService) {
+        this.cartaoService = cartaoService;
+    }
+
+    @PostMapping("/solicita/debito")
+    @SecurityRequirement(name = "barer-token")
+    public ResponseEntity<CartaoDebitoMostrarDadosResponse> solicitaCartaoDebito(
+            @Valid
+            @RequestBody CartaoDebitoCriarDadosRequest dados){
+        CartaoDebitoMostrarDadosResponse response = cartaoService.solicitaCartaoDebito(dados);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
