8ec8f0aad7d0a64a8afdcb5a46ca1ba2f00c3a6c
samuelmoreira05 <samuel442005@gmail.com>
2025-10-17 11:37:06 -0300

    fix: correcao na chamada do repository mudando os parametros de chamada

--- src/main/java/com/banco/api/banco/repository/CartaoRepository.java
+++ src/main/java/com/banco/api/banco/repository/CartaoRepository.java
@@ -2,9 +2,10 @@ package com.banco.api.banco.repository;
 
 import com.banco.api.banco.enums.TipoCartao;
 import com.banco.api.banco.model.entity.Cartao;
+import com.banco.api.banco.model.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 
-    boolean existsByContaAndTipoCartao(Cartao cartao, TipoCartao tipo);
+    boolean existsByContaAndTipoCartao(Conta conta, TipoCartao tipo);
 }
--- src/main/java/com/banco/api/banco/service/validadores/cartao/ValidadorQuantidadeCartao.java
+++ src/main/java/com/banco/api/banco/service/validadores/cartao/ValidadorQuantidadeCartao.java
@@ -1,4 +1,24 @@
 package com.banco.api.banco.service.validadores.cartao;
 
-public class ValidadorQuantidadeCartao {
+import com.banco.api.banco.enums.TipoCartao;
+import com.banco.api.banco.infra.exception.RegraDeNegocioException;
+import com.banco.api.banco.model.entity.Cartao;
+import com.banco.api.banco.model.entity.Cliente;
+import com.banco.api.banco.model.entity.Conta;
+import com.banco.api.banco.repository.CartaoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorQuantidadeCartao implements ValidadorSolicitacaoCredito{
+    private final CartaoRepository cartaoRepository;
+
+    public ValidadorQuantidadeCartao(CartaoRepository cartaoRepository) {
+        this.cartaoRepository = cartaoRepository;
+    }
+
+    public void validar(Cliente cliente, Conta conta){
+        if (cartaoRepository.existsByContaAndTipoCartao(conta, TipoCartao.CREDITO)){
+            throw new RegraDeNegocioException("O cliente ja tem um cartão de credito");
+        }
+    }
 }
